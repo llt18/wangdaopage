ffe7fbfff34bf8694e85f6fae6ec80a85840193e
llt18 <yzbld@outlook.com>
2026-02-06 16:14:20 +0800

    114514

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    *{
+      
+      padding:0;
+      margin:0;
+      
+}
+    body{
+      background-color:#F5F5DC;
+}
+    li{
+      list-style:none;
+}
+    .topdiv {
+      width:50%;
+      height:50px;
+      background-color:white;
+      margin:0 auto;
+      border-radius:10px;
+}  
+     .topdiv ul li {
+         
+        float:left;
+        margin-left:10px;      
+}
+    .topdiv ul{
+      display:flex;
+      justify-content:center;
+      
+}
+     .topdiv ul li {
+        font-size:35px;
+        color:gray;
+}
+
+</style>
+
+</head>
+
+<body>
+  
+  <div class="topdiv">
+    <ul>
+    <li> <a href="#">首页</a></li>
+    <li><a href="#">学校大事</a></li>
+    </ul> 
+  </div>
+</body>
+</html>
