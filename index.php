c0743ed20109196e3054c099d23151aa8c348e8f
llt18 <yzbld@outlook.com>
2026-02-06 17:12:08 +0800

    114514

--- index.php
+++ index.php
@@ -9,15 +9,18 @@
       
 }
     body{
-      background-color:#F5F5DC;
+      background-color:#FFFFF0;
 }
     li{
       list-style:none;
 }
     .topdiv {
+      position: relative;
       width:50%;
       height:50px;
       background-color:white;
+      display:flex;
+      justify-content:center;
       margin:0 auto;
       border-radius:10px;
 }  
@@ -30,15 +33,28 @@
       display:flex;
       justify-content:center;
       align-items:center;
+      
 }
      .topdiv ul li {
         font-size:25px;
-        color:gray;
+        
         
 }
       .topdiv ul li a{
         text-decoration:none;
+        
 }    
+      .logindiv{
+         
+        width:100px;
+        height:50px;
+        display:flex;
+        position:absolute;
+        right:15px;
+        transform:transformX(-50%);
+         
+}
+
 </style>
 
 </head>
@@ -50,7 +66,18 @@
     <li> <a href="#">首页</a></li>
     <li><a href="#">学校新闻</a></li>
     <li><a href="#">教师名片</a></li>
-    </ul> 
-  </div>
+    <li><a href="#">学习论坛</a><li>
+   </ul> 
+  
+<div class="logindiv" >
+  <ul>
+    <li><button>登陆</button></li>
+    <a href="#" style="font-size:5px;">还没账号?点我注册</a>
+  </ul>
+
+</div>
+   
+</div>
+</div>
 </body>
 </html>
