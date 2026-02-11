6426ac6a7cc78a22e10eee02a096e22abcb18b16
root <root@ser280136570487.local>
2026-02-11 08:14:49 +0000

    114514

--- login.php
+++ login.php
@@ -1,45 +1,34 @@
 <?php 
-session_start();
+error_reporting(E_ALL);
+ini_set("display_errors",1);
 
-?>
-<!doctype html>
-<html>
-<head></head>
-<body>
-   <?php 
-       
+session_start();
        $username=isset($_POST["username"]) ? $_POST["username"] : '';
        $password=isset($_POST["password"]) ? $_POST["password"] : '';
        $host='127.0.0.1';
-       $user='root';
-       $pass='';
-       $data='WANGDAOUSER';
+       $user='wangdao';
+       $pass='wangdao123';
+       $data='WANGDAOPAGE';
        $conn=new mysqli($host,$user,$pass,$data);
-       if($conn->connect_error<0){
+       if($conn->connect_error){
          die("connect error" . $conn->connect_error);
        }
        $username=$conn->real_escape_string($username);
-       //$password=$conn->real_escape_string($password);
-       $sql="SELECT * FROM WANGDAOUSER WHERE NAME= '$username';";
+       $password=$conn->real_escape_string($password);
+       $sql="SELECT * FROM WANGDAOUSER WHERE NAME= '$username' AND PASSWORD= '$password' ;";
        
        $result=$conn->query($sql);
        if($result->num_rows>0){
           $row=$result->fetch_assoc();
-          if($username == $row['NAME'] && $password == $row['PASSWORD']){
-            $_SESSION["islogin"]=true;
-            $_SESSION["username"]=$username;$_SESSION["password"]=$password;
-            echo "<script>alert('login successfuly');window.location.href='index.php';</script>";
-          }else{
-            $_SESSION["islogin"]=false;
-          }
+          $_SESSION["username"]=$row["NAME"];$_SESSION["islogin"]=true;header("Location: index.php");exit;
        }else{
-         $_SESSION["islogin"]=false;
-         
-echo "<script>alert('login error!');</script>";
-       }
+       $_SESSION["islogin"]=false;
+    
+       header("Location: login.php");
+      exit;
+}
        $conn->close();
 
 ?>
 
-</body>
-</html>
+
